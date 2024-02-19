f56bf087516d3cc3181aec2e074f7c6395999f31
RustyYato <rustyyato@gmail.com>
2024-02-19 12:04:04 -0700

    Optimize reader.clone

--- dbuf/src/interface.rs
+++ dbuf/src/interface.rs
@@ -1,5 +1,7 @@
 use core::ops;
 
+use alloc::borrow::Cow;
+
 pub(crate) type WriterId<S> = <S as Strategy>::WriterId;
 pub(crate) type ReaderId<S> = <S as Strategy>::ReaderId;
 
@@ -50,12 +52,16 @@ pub unsafe trait DoubleBufferReaderPointer: Clone {
     type Buffer;
     type Extras: ?Sized;
 
-    fn try_writer(&self) -> Result<Self::Writer, Self::UpgradeError>;
+    fn try_writer(&self) -> Result<Cow<'_, Self::Writer>, Self::UpgradeError>;
 }
 
 pub unsafe trait Strategy {
     type WriterId;
     type ReaderId;
+
+    type Swap;
+
+    type ReadGuard;
 
     // id constructors
 
@@ -66,6 +72,20 @@ pub unsafe trait Strategy {
     unsafe fn create_reader_id_from_reader(&self, reader: &Self::ReaderId) -> Self::ReaderId;
 
     unsafe fn create_invalid_reader_id() -> Self::ReaderId;
+
+    // swap handlers
+
+    unsafe fn start_swap(&self, writer: &mut Self::WriterId) -> Self::Swap;
+
+    unsafe fn is_swap_finished(&self, writer: &mut Self::WriterId, swap: &mut Self::Swap) -> bool;
+
+    unsafe fn finish_swap(&self, writer: &mut Self::WriterId, swap: Self::Swap);
+
+    // reader registration
+
+    unsafe fn acquire_read_guard(&self, reader: &Self::ReaderId) -> Self::ReadGuard;
+
+    unsafe fn release_read_guard(&self, reader: &Self::ReaderId, guard: Self::ReadGuard);
 }
 
 pub(crate) unsafe fn create_invalid_reader_id<S: Strategy>() -> S::ReaderId {
